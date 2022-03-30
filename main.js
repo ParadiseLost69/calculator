9feda2863ec8e1f88d02a09ca41b141c33278038
Teddy <tedloukas@outlook.com>
2022-03-30 17:58:01 +0900

    linked buttons in JS

--- main.js
+++ main.js
@@ -23,3 +23,23 @@ function operate (operator, num1, num2) {
     }
 
 }
+
+
+//Buttons
+let button0 = document.querySelector(".number-zero");
+let button1 = document.querySelector(".number-one");
+let button2 = document.querySelector(".number-two");
+let button3 = document.querySelector(".number-three");
+let button4 = document.querySelector(".number-four");
+let button5 = document.querySelector(".number-five");
+let button6 = document.querySelector(".number-six");
+let button7 = document.querySelector(".number-seven");
+let button8 = document.querySelector(".number-eight");
+let button9 = document.querySelector(".number-nine");
+ 
+let addButton = document.querySelector (".add")
+let subtractButton = document.querySelector (".subtract")
+let multiplyButton = document.querySelector (".multiply")
+let divideButton = document.querySelector (".divide")
+ 
+let onButton = document.querySelector (".on-button")
