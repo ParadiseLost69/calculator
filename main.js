1be076927d7a1fc104d67ffca4634b998b218c56
Teddy <tedloukas@outlook.com>
2022-03-30 17:43:34 +0900

    added buttons

--- main.js
+++ main.js
@@ -10,4 +10,16 @@ let multiply = (num1,num2) => num1 * num2;
 //divide
 let divide = (num1,num2) => num1/num2;
 
+function operate (operator, num1, num2) {
+    if (operator === "+" ) {
+        return sum(num1, num2)
+        
+    } else if (operator === "-") {
+        return diff(num1,num2)
+    } else if (operator === "*") {
+        return multiply(num1,num2) 
+    } else if (operator === "/") {
+        return divide(num1,num2)
+    }
 
+}
