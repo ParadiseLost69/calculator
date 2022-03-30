b8f77c0fa6f061cb26295e63f67e86f248a10330
Teddy <tedloukas@outlook.com>
2022-03-30 17:10:34 +0900

    added plus minus multiply and divide functions main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+//Addition Function
+let sum = (num1,num2) => num1+num2;
+
+//Subtraction
+let diff = (num1,num2) => num1-num2;
+
+//multiply
+let multiply = (num1,num2) => num1 * num2;
+
+//divide
+let divide = (num1,num2) => num1/num2;
+
+
